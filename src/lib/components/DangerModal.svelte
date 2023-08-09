e97f091fe9bf12d5c9f8252fc6d3f943d54862e7
Mario Perez Hurtado <marioph10@gmail.com>
2023-08-09 11:08:26 +0200

    Add danger modal

---
+++ src/lib/components/DangerModal.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	import { scale } from 'svelte/transition';
+
+	export let title: string;
+	export let description: string;
+	export let cancelText: string;
+	export let confirmText: string;
+
+	export let isOpen: boolean;
+	export let onConfirm: () => void;
+</script>
+
+{#if isOpen}
+	<div class="fixed top-0 left-0 w-screen h-screen cursor-pointer bg-dark/70" />
+
+	<div
+		in:scale
+		class="fixed max-w-md p-4 -translate-x-1/2 rounded-sm shadow-md bottom-1/2 left-1/2 bg-dark-3"
+	>
+		<h1 class="mb-5 text-base">{title}</h1>
+		<p>{description}</p>
+		<div class="flex justify-end gap-2 mt-5 text-sm">
+			<button
+				on:click={() => (isOpen = false)}
+				class="block px-3 py-1.5 rounded-sm shadow-md bg-dark-2"
+			>
+				{cancelText}
+			</button>
+			<button on:click={onConfirm} class="block px-3 py-1.5 rounded-sm shadow-md bg-accent">
+				{confirmText}
+			</button>
+		</div>
+	</div>
+{/if}
--- src/routes/FileExplorer/FileActions.svelte
+++ src/routes/FileExplorer/FileActions.svelte
@@ -4,9 +4,12 @@
 	import ContextMenu from '$lib/components/ContextMenu/ContextMenu.svelte';
 	import ContextMenuItem from '$lib/components/ContextMenu/ContextMenuItem.svelte';
 	import ContextMenuSeparator from '$lib/components/ContextMenu/ContextMenuSeparator.svelte';
+	import DangerModal from '$lib/components/DangerModal.svelte';
 
 	export let file: TFile;
 	export let isOpen: boolean;
+
+	let confirmingDelete = false;
 
 	async function handleDelete() {
 		if (!$rootFolder?.folder) return;
@@ -25,5 +28,18 @@
 	<ContextMenuSeparator />
 
 	<ContextMenuItem title="Rename" command="R" action={handleDelete} />
-	<ContextMenuItem title="Delete permanently" command="D" action={handleDelete} />
+	<ContextMenuItem
+		title="Delete permanently"
+		command="D"
+		action={() => (confirmingDelete = true)}
+	/>
 </ContextMenu>
+
+<DangerModal
+	isOpen={confirmingDelete}
+	onConfirm={handleDelete}
+	title="Are you sure you want to delete '{file.file.name}' permanently?"
+	description="This action cannot be undone."
+	cancelText="Cancel"
+	confirmText="Delete"
+/>
